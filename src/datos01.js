d0ef4e4ff0214866a3cda44bd1c83cee1bef3f6d
Ismael <igarciav@iespuntadelverde.es>
2020-11-08 13:13:44 +0100

    First commit

---
+++ src/datos01.js
@@ -0,0 +1,14 @@
+db.employees.drop()
+db.employees.insertMany([
+	{firstname:"Fathima", lastname:"Moham", birth:"1991-03-17", gender:"F", checkin:912, email:"fathimamoham17@hotmail.com", phone:"900210499"},
+	{firstname:"David", lastname:"Lopez", birth:"1998-06-23", gender:"M", checkin:211, email:"lopez_david@gmail.com", phone:"617982546"},
+	{firstname:"Maria", lastname:"Perez", birth:"2000-03-10", gender:"F", checkin:108, email:"mariaperez0@gmail.com", phone:"678245093"},
+	{firstname:"Michael", lastname:"Muller", birth:"1987-09-20", gender:"M", checkin:2034, email:"mullerm@hotmail.com", phone:"927839109"},	
+	{firstname:"Francisco", lastname:"Garcia", birth:"2001-10-10", gender:"M", checkin:58, email:"frangar@gmail.com", phone:"656674129"},	
+	{firstname:"Pedro", lastname:"Villalba", birth:"1992-01-15", gender:"M", checkin:1093, email:"villalbapedro15@hotmail.com", phone:"617893524"},	
+	{firstname:"Raquel", lastname:"Bermudez", birth:"1979-07-30", gender:"F",  checkin:5032, email:"raquelbermu@gmail.com", phone:"671920938"},	
+	{firstname:"Radhica", lastname:"Sharma", birth:"1999-01-28", gender:"F", checkin:183,email:"radhicash01@gmail.com", phone:"918029374"},	
+	{firstname:"Daniel", lastname:"Ramos", birth:"1998-11-22", gender:"M", checkin:210, email:"danielramos@hotmail.com", phone:"678190203"},	
+	{firstname:"Marcos", lastname:"Rossi", birth:"1992-10-09", gender:"M", checkin:801, email:"rossimarc09@gmail.com", phone:"690186742"},	
+	{firstname:"Patricia", lastname:"Garcia", birth:"2000-01-20", gender:"F", checkin:120, email:"patriciagarcia@gmail.com", phone:"691126542"}	
+]);
